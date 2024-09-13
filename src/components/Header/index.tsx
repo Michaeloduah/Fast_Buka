81e46921d4a4578b357819ae828afeb256fc982f
Michael Oduah <oduahmichaelebuka@gmail.com>
2024-09-13 16:17:36 +0100

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import useColorMode  from "@/hooks/useColorMode"
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
 import Logo_Dark from "../../../public/images/homepage/logo-dark.png"
@@ -13,7 +12,6 @@ const Header = (
     setSidebarOpen: (arg0: boolean) => void;
   }
 ) => {
-  const [colorMode] = useColorMode();
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
